3fc9ccf496c05e13395118fe1214cecc1d2a8be6
vbroussard <vbroussard@users.noreply.github.com>
2013-08-22 14:57:04 +0000

    DIRECTORY-199 : Correction of bootstrap integration
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/directory-form-workflow/module-directory-pdfproducer-archive@50979 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/sql/plugins/directory/modules/pdfproducer/archive/upgrade/update_db_directory_pdfproducer_archive-2.0.0-2.0.1.sql
@@ -0,0 +1,8 @@
+UPDATE directory_zip_basket_action SET icon_url = 'icon-search' WHERE id_action = 1;
+UPDATE directory_zip_basket_action SET icon_url = 'icon-search' WHERE id_action = 2;
+UPDATE directory_zip_basket_action SET icon_url = 'icon-search' WHERE id_action = 3;
+UPDATE directory_zip_basket_action SET icon_url = 'icon-search' WHERE id_action = 4;
+UPDATE directory_zip_basket_action SET icon_url = 'icon-download' WHERE id_action = 5;
+UPDATE directory_zip_basket_action SET icon_url = 'icon-trash' WHERE id_action = 6;
+UPDATE directory_zip_basket_action SET icon_url = 'icon-trash' WHERE id_action = 7;
+UPDATE directory_zip_basket_action SET icon_url = 'icon-trash' WHERE id_action = 8;
